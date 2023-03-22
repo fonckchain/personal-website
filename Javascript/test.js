b95d98a73d38545d5e8b5389fc624d533d8ff6b9
Fonck <AFonck@protonmail.com>
2023-03-22 04:57:35 -0600

    test.

--- Javascript/test.js
+++ Javascript/test.js
@@ -1,20 +1,38 @@
-//let name = 'Fonck';
-//let age = [25, 30];
-//let person = {
+// let name = 'Fonck';
+// let age = [25, 30];
+// let person = {
 //    name: 'Lilia',
 //    age: 28
-//};
-//console.log(person);
-//console.log(age[1]);
+// };
+// console.log(person);
+// console.log(age[1]);
 
-function greet (name) {
-    console.log("I'm gonna visit your aussie ass once $GME pops " + name + " , I hope you're ready");
-}
-greet('Sgnt');
+// function greet (name) {
+//     console.log("I'm gonna visit your aussie ass once $GME pops " + name + " , I hope you're ready");
+// }
+// greet('Sgnt');
 
-function MOASS(shares) {
-    return shares * 1181561516548191515651618948498;
-}
+// function MOASS(shares) {
+//     return shares * 1181561516548191515651618948498;
+// }
 
-let shares = MOASS(2);
-console.log(' 2 shares sold for $' + MOASS(2));
+// let shares = MOASS(2);
+// console.log(' 2 shares sold for $' + MOASS(2));
+
+let shares = 4;
+let loan = 2;
+let price = 5e+500;
+
+let x = shares;
+x = x + 5;
+x += 5;
+
+console.log(shares * price);
+console.log(shares - loan);
+console.log(shares ** loan);
+console.log(shares % loan);
+console.log(loan + shares * price);
+console.log(shares > loan);
+
+let type = shares > 3 ? 'whale' : 'fish';
+console.log(type);
