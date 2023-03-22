f11b0a65b1e3f63e03a6dbb61ef94087f29f193c
Fonck <AFonck@protonmail.com>
2023-03-21 21:24:29 -0600

    Java

---
+++ Javascript/test.js
@@ -0,0 +1,20 @@
+//let name = 'Fonck';
+//let age = [25, 30];
+//let person = {
+//    name: 'Lilia',
+//    age: 28
+//};
+//console.log(person);
+//console.log(age[1]);
+
+function greet (name) {
+    console.log("I'm gonna visit your aussie ass once $GME pops " + name + " , I hope you're ready");
+}
+greet('Sgnt');
+
+function MOASS(shares) {
+    return shares * 1181561516548191515651618948498;
+}
+
+let shares = MOASS(2);
+console.log(' 2 shares sold for $' + MOASS(2));
