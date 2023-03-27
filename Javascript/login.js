e3cf6b4b0e0f585de24e11f4b67d786a31a78fe7
Fonck <AFonck@protonmail.com>
2023-03-27 11:17:16 -0600

    registration prompt fix.

--- Javascript/login.js
+++ Javascript/login.js
@@ -9,48 +9,14 @@ const rememberMeCheckbox = document.querySelector('input[name="remember-me"]');
 const forgotPasswordForm = document.querySelector('.forgot-password-page');
 const registrationForm = document.querySelector('.registration-page');
 
-
 // Check if the user has already logged in
 const loggedInUser = localStorage.getItem('loggedInUser');
 if (loggedInUser) {
-  // Auto-fill the username input with the saved username
   loginUsernameInput.value = loggedInUser;
-
-  // Check the "Remember me" checkbox
   rememberMeCheckbox.checked = true;
 }
 
-// Define a function to handle successful logins
-function handleLoginSuccess(username, event) {
-  // Save the username to local storage if the "Remember me" checkbox is checked
-  if (rememberMeCheckbox.checked) {
-    localStorage.setItem('loggedInUser', username);
-  } else {
-    localStorage.removeItem('loggedInUser');
-    loginUsernameInput.value = ''; // Clear the username input if "Remember me" is unchecked
-  }
-
-  // Redirect the user to the home page or dashboard if the login button was clicked
-  if (event.target.classList.contains('login-btn')) {
-    window.location.href = '../html/home.html';
-  }
-}
-
-// Define a function to handle failed logins
-function handleLoginFailure() {
-  alert('Invalid username or password.');
-}
-
-// =============================================================================
-// CLOSE BUTTON
-// =============================================================================
-
-// Create the close button element
-const closeButton = document.createElement('button');
-closeButton.classList.add('close-button');
-closeButton.innerHTML = '&times;';
-
-// Attach click event listeners to the login buttons
+// Attach event listeners to the login buttons
 loginBtns.forEach(loginBtn => {
   loginBtn.addEventListener('click', function() {
     // Display the login form
@@ -58,6 +24,9 @@ loginBtns.forEach(loginBtn => {
     loginForm.classList.add('popup');
 
     // Append the close button to the pop-up window
+    const closeButton = document.createElement('button');
+    closeButton.classList.add('close-button');
+    closeButton.innerHTML = '&times;';
     loginForm.appendChild(closeButton);
 
     // Add a click event listener to the close button
@@ -68,17 +37,17 @@ loginBtns.forEach(loginBtn => {
   });
 });
 
-// Attach click event listeners to the register and forgot password buttons
+// Attach event listeners to the register and forgot password buttons
 registerBtn.addEventListener('click', function() {
-  // Display the registration form
   loginForm.style.display = 'none';
   registrationForm.style.display = 'block';
   registrationForm.classList.add('popup');
 
-  // Append the close button to the pop-up window
+  const closeButton = document.createElement('button');
+  closeButton.classList.add('close-button');
+  closeButton.innerHTML = '&times;';
   registrationForm.appendChild(closeButton);
 
-  // Add a click event listener to the close button
   closeButton.addEventListener('click', function() {
     registrationForm.classList.remove('popup');
     registrationForm.style.display = 'none';
@@ -86,39 +55,53 @@ registerBtn.addEventListener('click', function() {
 });
 
 forgotPasswordBtn.addEventListener('click', function() {
-  // Display the forgot password form
   loginForm.style.display = 'none';
   forgotPasswordForm.style.display = 'block';
   forgotPasswordForm.classList.add('popup');
 
-  // Append the close button to the pop-up window
+  const closeButton = document.createElement('button');
+  closeButton.classList.add('close-button');
+  closeButton.innerHTML = '&times;';
   forgotPasswordForm.appendChild(closeButton);
 
-  // Add a click event listener to the close button
   closeButton.addEventListener('click', function() {
     forgotPasswordForm.style.display = 'none';
     forgotPasswordForm.classList.remove('popup');
   });
 });
 
+// Handle successful logins
+function handleLoginSuccess(username, event) {
+  if (rememberMeCheckbox.checked) {
+    localStorage.setItem('loggedInUser', username);
+  } else {
+    localStorage.removeItem('loggedInUser');
+    loginUsernameInput.value = '';
+  }
 
+  if (event.target.classList.contains('login-btn')) {
+    window.location.href = '../html/home.html';
+  }
+}
 
+// Handle failed logins
+function handleLoginFailure(event) {
+  alert('Invalid username or password.');
+  event.preventDefault();
+}
 
-
-// Attach a submit event listener to the login form
+// Attach submit event listener to login form
 loginForm.addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevent the form from submitting normally
-
-  // Get the username and password inputs
+  event.preventDefault();
   const username = loginUsernameInput.value;
   const password = loginPasswordInput.value;
 
-  // TODO: Add code to check the credentials against a database or API
-
-  // For now, just simulate a successful login if the username and password match
+  // Check credentials against a database or API
   if (username === 'testuser' && password === 'testpassword') {
     handleLoginSuccess(username, event);
   } else {
-    handleLoginFailure(event);
+    if (event.submitter.classList.contains('login-btn')) { // Check if the submit button is the login button
+      handleLoginFailure();
+    }
   }
 });
