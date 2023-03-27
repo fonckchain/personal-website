d788d29668f59ae4c53f1da7224361e4a9721644
Fonck <AFonck@protonmail.com>
2023-03-27 11:17:06 -0600

    register prompt fix.

--- Javascript/back-up.js
+++ Javascript/back-up.js
@@ -102,9 +102,6 @@ forgotPasswordBtn.addEventListener('click', function() {
 });
 
 
-
-
-
 // Attach a submit event listener to the login form
 loginForm.addEventListener('submit', function(event) {
   event.preventDefault(); // Prevent the form from submitting normally
