c5c00de55fcd191b18082251dc68ddae268d95f5
Fonck <AFonck@protonmail.com>
2023-03-30 20:21:49 -0600

    Header & footer update

--- Javascript/menu.js
+++ Javascript/menu.js
@@ -1,7 +1,7 @@
+const toggleBtn = document.querySelector(".toggle-btn");
+const toggleBtnIcon = document.querySelector(".toggle-btn i");
+const dropDownMenu = document.querySelector(".dropdown-menu");
 
-toggleBtn = document.querySelector(".toggle-btn");
-toggleBtnIcon = document.querySelector(".toggle-btn i");
-dropDownMenu = document.querySelector(".dropdown-menu");
 toggleBtn.onclick = function () {
   dropDownMenu.classList.toggle("open");
   const isOpen = dropDownMenu.classList.contains("open");
@@ -9,3 +9,13 @@ toggleBtn.onclick = function () {
     ? "fa-solid fa-xmark"
     : "fa-solid fa-bars";
 };
+
+document.addEventListener('click', function (event) {
+  const isClickInside = dropDownMenu.contains(event.target);
+  const isToggleBtn = event.target === toggleBtn || toggleBtn.contains(event.target);
+
+  if (!isClickInside && !isToggleBtn && dropDownMenu.classList.contains("open")) {
+    dropDownMenu.classList.remove("open");
+    toggleBtnIcon.classList = "fa-solid fa-bars";
+  }
+});
