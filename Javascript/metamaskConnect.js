2af35ff84241a566e5cc77f5708cff09df825338
Fonck <AFonck@protonmail.com>
2023-04-04 07:52:05 -0600

    Connect only to metamask.

--- Javascript/metamaskConnect.js
+++ Javascript/metamaskConnect.js
@@ -1,25 +1,34 @@
-async function connectWithMetaMask() {
-    const errorMessageElement = document.getElementById("error-message");
-    const errorModal = document.getElementById("error-modal");
-    const closeModal = document.getElementById("close-modal");
-  
-    closeModal.onclick = function () {
-      errorModal.style.display = "none";
-    };
-  
-    if (typeof window.ethereum !== "undefined") {
-      try {
-        await window.ethereum.request({ method: "eth_requestAccounts" });
-        errorMessageElement.innerText = ""; // Clear the error message
-        errorModal.style.display = "none";
-      } catch (error) {
-        errorMessageElement.innerText = "User denied account access";
-        errorModal.style.display = "block";
-      }
-    } else {
-      errorMessageElement.innerText =
-        "MetaMask not detected. Please install the MetaMask extension or use a compatible browser.";
-      errorModal.style.display = "block";
-    }
+async function detectMetaMaskProvider() {
+  if (typeof window.ethereum !== "undefined" && window.ethereum.isMetaMask) {
+    return window.ethereum;
   }
-  
+  return null;
+}
+
+async function connectWithMetaMask() {
+  const errorMessageElement = document.getElementById("error-message");
+  const errorModal = document.getElementById("error-modal");
+  const closeModal = document.getElementById("close-modal");
+
+  closeModal.onclick = function () {
+    errorModal.style.display = "none";
+  };
+
+  const provider = await detectMetaMaskProvider();
+
+  if (!provider) {
+    errorMessageElement.innerText =
+      "MetaMask not detected. Please install the MetaMask extension or use a compatible browser.";
+    errorModal.style.display = "block";
+    return;
+  }
+
+  try {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+    errorMessageElement.innerText = ""; // Clear the error message
+    errorModal.style.display = "none";
+  } catch (error) {
+    errorMessageElement.innerText = "User denied account access";
+    errorModal.style.display = "block";
+  }
+}
